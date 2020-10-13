1e16ce7fd76afbfbceafee057716bcf62a738bdc
lomi <lomi@lomag.com>
2020-10-13 14:30:01 +0200

    Basic user implemented

--- dj_casaLomage/settings.py
+++ dj_casaLomage/settings.py
@@ -36,7 +36,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "autoslug",
+    "crispy_forms",
     "recipes.apps.RecipesConfig",
+    "users.apps.UsersConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Cryspy config
+CRISPY_TEMPLATE_PACK = "bootstrap4"
