b3250a14022c93fc53801deabbcae6da7d85e91b
lomi <lomi@lomag.com>
2020-10-13 18:16:35 +0200

    login and out

--- dj_casaLomage/settings.py
+++ dj_casaLomage/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'Europe/Paris'
 
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 
 # Cryspy config
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# Login config
+LOGIN_REDIRECT_URL = "recipe-home"
+LOGIN_URL = "login"
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == "POST":
@@ -10,7 +12,11 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Compte créé pour {username}")
-            return redirect("recipe-home")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
